d74e83b1ca6239ce0d0ef6c810840b158269768b
caitaozhan <caitaozhan@gmail.com>
2019-11-19 21:56:22 -0500

    278 done

---
+++ 278.first-bad-version.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=278 lang=python3
+#
+# [278] First Bad Version
+#
+# https://leetcode.com/problems/first-bad-version/description/
+#
+# algorithms
+# Easy (30.78%)
+# Likes:    844
+# Dislikes: 487
+# Total Accepted:    270.9K
+# Total Submissions: 846.5K
+# Testcase Example:  '5\n4'
+#
+# You are a product manager and currently leading a team to develop a new
+# product. Unfortunately, the latest version of your product fails the quality
+# check. Since each version is developed based on the previous version, all the
+# versions after a bad version are also bad.
+# 
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first
+# bad one, which causes all the following ones to be bad.
+# 
+# You are given an API bool isBadVersion(version) which will return whether
+# version is bad. Implement a function to find the first bad version. You
+# should minimize the number of calls to the API.
+# 
+# Example:
+# 
+# 
+# Given n = 5, and version = 4 is the first bad version.
+# 
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+# 
+# Then 4 is the first bad version. 
+# 
+# 
+#
+
+# @lc code=start
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+def isBadVersion(version):
+    return True
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            bad = isBadVersion(mid)
+            if bad is False:
+                left = mid + 1
+            else:
+                right = mid
+        return right
+
+
+# @lc code=end
+
