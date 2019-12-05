4f353411990eb47abbffeac50a252fcbd6477d5d
caitaozhan <caitaozhan@gmail.com>
2019-12-04 19:39:57 -0500

    1143 done

---
+++ 1143.longest-common-subsequence.py
@@ -0,0 +1,80 @@
+#
+# @lc app=leetcode id=1143 lang=python3
+#
+# [1143] Longest Common Subsequence
+#
+# https://leetcode.com/problems/longest-common-subsequence/description/
+#
+# algorithms
+# Medium (57.32%)
+# Likes:    382
+# Dislikes: 10
+# Total Accepted:    23.5K
+# Total Submissions: 40.9K
+# Testcase Example:  '"abcde"\n"ace"'
+#
+# Given two strings text1 and text2, return the length of their longest common
+# subsequence.
+# 
+# A subsequence of a string is a new string generated from the original string
+# with some characters(can be none) deleted without changing the relative order
+# of the remaining characters. (eg, "ace" is a subsequence of "abcde" while
+# "aec" is not). A common subsequence of two strings is a subsequence that is
+# common to both strings.
+# 
+# 
+# 
+# If there is no common subsequence, return 0.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: text1 = "abcde", text2 = "ace" 
+# Output: 3  
+# Explanation: The longest common subsequence is "ace" and its length is 3.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: text1 = "abc", text2 = "abc"
+# Output: 3
+# Explanation: The longest common subsequence is "abc" and its length is 3.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: text1 = "abc", text2 = "def"
+# Output: 0
+# Explanation: There is no such common subsequence, so the result is 0.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= text1.length <= 1000
+# 1 <= text2.length <= 1000
+# The input strings consist of lowercase English characters only.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[n][m]
+        
+# @lc code=end
+
