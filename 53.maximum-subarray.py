b1210f2a0de0d5eb477ad685379bba91e7e97755
caitaozhan <caitaozhan@gmail.com>
2019-12-13 14:59:15 -0500

    53 done

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+# https://leetcode.com/problems/maximum-subarray/description/
+#
+# algorithms
+# Easy (45.12%)
+# Likes:    5703
+# Dislikes: 239
+# Total Accepted:    709K
+# Total Submissions: 1.6M
+# Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+#
+# Given an integer array nums, find the contiguous subarray (containing at
+# least one number) which has the largest sum and return its sum.
+# 
+# Example:
+# 
+# 
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+# 
+# 
+# Follow up:
+# 
+# If you have figured out the O(n) solution, try coding another solution using
+# the divide and conquer approach, which is more subtle.
+# 
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    '''dynamic programming
+    '''
+    def maxSubArray(self, nums: List[int]) -> int:
+        nums.insert(0, 0)
+        dp = [0 for _ in range(len(nums))]
+        maxx = -2**64
+        for i in range(1, len(nums)):
+            if dp[i-1] >= 0:
+                dp[i] = dp[i-1] + nums[i]
+            else:
+                dp[i] = nums[i]
+            maxx = max(dp[i], maxx)
+        return maxx
+# @lc code=end
+
