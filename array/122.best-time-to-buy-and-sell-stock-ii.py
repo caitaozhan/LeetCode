9673ee719b2ac02726efaaaa4d8d76080b063f02
caitaozhan <caitaozhan@gmail.com>
2020-08-18 11:22:42 -0400

    122

---
+++ array/122.best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=122 lang=python3
+#
+# [122] Best Time to Buy and Sell Stock II
+#
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+#
+# algorithms
+# Easy (56.28%)
+# Likes:    2704
+# Dislikes: 1785
+# Total Accepted:    644K
+# Total Submissions: 1.1M
+# Testcase Example:  '[7,1,5,3,6,4]'
+#
+# Say you have an array prices for which the i^th element is the price of a
+# given stock on day i.
+# 
+# Design an algorithm to find the maximum profit. You may complete as many
+# transactions as you like (i.e., buy one and sell one share of the stock
+# multiple times).
+# 
+# Note: You may not engage in multiple transactions at the same time (i.e., you
+# must sell the stock before you buy again).
+# 
+# Example 1:
+# 
+# 
+# Input: [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit =
+# 5-1 = 4.
+# Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+# 3.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit =
+# 5-1 = 4.
+# Note that you cannot buy on day 1, buy on day 2 and sell them later, as you
+# are
+# engaging multiple transactions at the same time. You must sell before buying
+# again.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= prices.length <= 3 * 10 ^ 4
+# 0 <= prices[i] <= 10 ^ 4
+# 
+# 
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        prices.append(-1)
+        state = 'up' if prices[1] >= prices[0] else 'down'
+        if state == 'up':
+            buy = prices[0]
+        pre = prices[1]
+        profit = 0
+        for cur in prices[2:]:
+            if state == 'down' and cur >= pre:   # time to buy
+                buy = pre
+                state = 'up'
+            if state == 'up' and cur < pre:     # time to sell
+                sell = pre
+                profit += sell - buy
+                state = 'down'
+            pre = cur
+        return profit
+# @lc code=end
+
