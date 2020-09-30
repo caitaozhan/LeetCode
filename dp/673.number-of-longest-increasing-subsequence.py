5646e5061b418b94f95e24022344163edf75dea6
caitaozhan <caitaozhan@gmail.com>
2020-09-30 13:58:27 -0400

    673

--- dp/673.number-of-longest-increasing-subsequence.py
+++ dp/673.number-of-longest-increasing-subsequence.py
@@ -23,7 +23,29 @@ from typing import List
 
 # @lc code=start
 class Solution:
+    '''Comparing to Solution2, same time complexity, but code level optimization.
+    '''
     def findNumberOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp1 = [1]*len(nums)  # dp1[i] is the length of the longest increasing subsequence that ends at nums[i]
+        dp2 = [1]*len(nums)  # dp2[i] is the number of longest increasing subsequence whose length is dp1[i] and ends at nums[i]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if dp1[j] + 1 > dp1[i]:
+                        dp1[i] = dp1[j] + 1
+                        dp2[i] = dp2[j]
+                    elif dp1[j] + 1 == dp1[i]:
+                        dp2[i] += dp2[j]
+        maxx = max(dp1)
+        return sum(b for a, b in zip(dp1, dp2) if a == maxx)
+
+
+class Solution2:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        if not nums:   # one BUG here
+            return 0
         dp1 = [1]*len(nums)
         dp2 = [1]*len(nums)
         for i in range(len(nums)):
@@ -34,7 +56,7 @@ class Solution:
                 continue
             summ = 0
             for j in range(i):
-                if dp1[j] == dp1[i] - 1:
+                if nums[i] > nums[j] and dp1[j] == dp1[i] - 1:  # one BUG here, forgot nums[i] > nums[j]
                     summ += dp2[j]
             dp2[i] = summ
 
@@ -43,9 +65,8 @@ class Solution:
         for i in range(len(dp2)):
             if dp1[i] == maxx:
                 summ += dp2[i]
-        print(dp1)
-        print(dp2)
         return summ
+
 
 # nums = [1,3,5,4,7]
 # nums = [2,2,2,2,2]
