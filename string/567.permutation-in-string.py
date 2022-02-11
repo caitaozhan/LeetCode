986a7c2005ccd71918145db594aa5a5fc56691c0
Caitao Zhan <caitaozhan@gmail.com>
2022-02-11 11:40:35 -0500

    567

---
+++ string/567.permutation-in-string.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=567 lang=python3
+#
+# [567] Permutation in String
+#
+
+from collections import Counter
+
+# @lc code=start
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1counter = Counter(s1)
+        N = len(s1)
+        s2counter = Counter(s2[:N])
+        if s1counter == s2counter:
+            return True
+        for i in range(N, len(s2)):
+            s2counter[s2[i - N]] -= 1
+            s2counter[s2[i]] += 1
+            if s1counter == s2counter:
+                return True
+        return False
+
+
+# @lc code=end
+
