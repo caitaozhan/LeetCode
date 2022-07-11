b096f61448e1cca01822848939569df554693a19
Caitao Zhan <caitaozhan@gmail.com>
2022-07-11 10:56:19 -0400

    199

---
+++ queue/199.binary-tree-right-side-view.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        ans = []
+        queue = [root]
+        while queue:
+            ans.append(queue[-1].val)
+            new_queue = []
+            for node in queue:
+                if node.left:
+                    new_queue.append(node.left)
+                if node.right:
+                    new_queue.append(node.right)
+            queue = new_queue
+
+        return ans
+
+# @lc code=end
+
