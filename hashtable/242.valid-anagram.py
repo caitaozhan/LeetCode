dbd9b0fbaf01cb3172fd270c6b6e6b14f5c97b1b
Caitao Zhan <caitaozhan@gmail.com>
2022-07-28 10:15:30 -0400

    242

---
+++ hashtable/242.valid-anagram.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+# @lc code=end
+
