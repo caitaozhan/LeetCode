6fccf63698125e8385e4d06ae16925090b61bb22
Caitao Zhan <caitaozhan@gmail.com>
2023-01-09 11:54:12 -0500

    144

---
+++ tree/144.binary-tree-preorder-traversal.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    '''using recursion / divide and conquer
+    '''
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+
+class Solution:
+    '''using a stack
+    '''
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                ans.append(cur.val)
+                cur = cur.left
+            cur = stack.pop()
+            cur = cur.right
+        return ans
+            
+
+
+# @lc code=end
+
