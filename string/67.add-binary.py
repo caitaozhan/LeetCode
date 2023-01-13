32943500ec52b07eb0e86fae75ebb7df6b69c3df
caitaozhan <caitaozhan@gmail.com>
2023-01-13 17:03:23 -0500

    67

---
+++ string/67.add-binary.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=67 lang=python3
+#
+# [67] Add Binary
+#
+
+# @lc code=start
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = int(a, 2)
+        b = int(b, 2)
+        return bin(a + b)[2:]
+        
+# @lc code=end
+
